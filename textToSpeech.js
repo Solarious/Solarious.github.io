28b5d15eb29feb1cd77224640e045706e121c7dc
Nathan Aplin <solariousz@gmail.com>
2015-05-13 21:22:24 +0800

    Added text to speech prototype

---
+++ textToSpeech.js
@@ -0,0 +1,33 @@
+function speakJap(text) {
+	if (browserSupportsNaiveSpeech()) {
+		speakJapNaive(text);
+	} else {
+		speakJapGoogle(text);
+	}
+}
+
+function browserSupportsNaiveSpeech() {
+	return ('speechSynthesis' in window);
+}
+
+function speakJapNaive(text) {
+	var u = new SpeechSynthesisUtterance();
+	u.text = text;
+	u.lang = 'ja-JP';
+	u.rate = 1;
+	u.onerror = function(e) {
+		alert("speakJap had an error");
+	}
+	speechSynthesis.speak(u);
+}
+
+function speakJapGoogle(text) {
+	var audio = new Audio();
+	var audioText = 'http://translate.google.com/translate_tts?ie=utf-8&tl=ja&q=' + encodeURIComponent(text);
+	audio.src = audioText;
+	audio.play();
+}
+
+function speakFromText() {
+	speakJap(document.getElementById("textBox").value);
+}
